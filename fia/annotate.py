0ef3616562efa5974c371af7465cf06bd143c615
Elad Noor <elad.noor@gmail.com>
2015-11-18 18:33:51 +0100

    adding scripts for downloading and annotation general data from FIA-TOF

---
+++ fia/annotate.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 23:54:03 2015
+
+@author: eladn
+"""
+
+import numpy as np
+import sys
+import csv
+import argparse
+from progressbar import ProgressBar
+from openbis import download_data_profiles
+
+# script parameters (as determined by Mattia)
+MIN_PEAK_SIZE = 5000
+MAX_MZ_DIFFERENCE = 0.003
+MAX_REF_MASS = 1000
+REFERENCE_MASS_FNAME = 'EMDTB.csv'
+
+def findpeaks(a):
+    """
+    Returns:
+        list of indices of the local maxima in a 1D array 'a'. A local peak is 
+        larger than its two neighboring samples. Endpoints are excluded.
+        If a peak is flat, the function returns only the point with the lowest index.
+    """
+    tmp = np.diff(np.sign(np.diff(a.flat)))
+    return np.where(tmp == -2)[0] + 1
+
+parser = argparse.ArgumentParser(description='Download FIA raw data from openBIS')
+parser.add_argument('exp_code', type=str,
+                    help='the openBIS experiment ID')
+parser.add_argument('-o', dest='output_fname', type=str, default=None,
+                    help='a filename for writing the output')
+args = parser.parse_args()
+
+dsCode, dsSampleCode, dataProfiles = download_data_profiles(args.exp_code)
+n_samples = len(dataProfiles)
+
+# all_peaks is a list of matrices (one per sample) of the ion mz and intensity
+# only for the peaks (i.e. local maxima)
+all_peaks = []
+
+#%% identify peaks (local maxima)
+sys.stderr.write('\nCentroids identification\n')
+with ProgressBar(max_value=n_samples) as progress:
+    for i, dataset in enumerate(dataProfiles):
+        # find all the values that are local maxima and pass the threshold
+        idxs = findpeaks(dataset[:, 1])
+        idxs = filter(lambda j : dataset[j, 1] >= MIN_PEAK_SIZE, idxs)
+        all_peaks.append(dataset[idxs, :])
+        progress.update(i)
+
+#%% Use the reference table to associate peaks to compounds, by minimum mass distance
+sys.stderr.write('\nMetabolites identification\n')
+
+with open(REFERENCE_MASS_FNAME, 'r') as fp:
+    # we didn't measure masses above 1000 Da
+    reference_mass_dlist = filter(lambda x:float(x['mass']) < MAX_REF_MASS, csv.DictReader(fp))
+    reference_masses = np.array(map(lambda x: float(x['mass']), reference_mass_dlist))
+    reference_masses -= reference_masses[0] # subtract the mass of H+ (i.e. look for deprotonated masses)
+
+# peak_masses[i, j] will contain the exact mass of the peak which is closest
+# to reference 'j' in sample 'i'. If there is no peak which is close enough
+# (i.e. in the range of MAX_MZ_DIFFERENCE), the value will be NaN
+# peak_indices[i, j] will contain the index of that peak in 'all_peaks[i]'
+peak_masses = np.zeros((n_samples, len(reference_mass_dlist)), dtype=np.single)
+peak_indices = np.zeros((n_samples, len(reference_mass_dlist)), dtype=int)
+with ProgressBar(max_value=n_samples) as progress:
+    for i in xrange(n_samples):
+        for j, refmass in enumerate(reference_masses):
+            diffs = abs(all_peaks[i][:, 0] - refmass)
+            peak_idx = np.argmin(diffs)
+            if diffs[peak_idx] <= MAX_MZ_DIFFERENCE:
+                peak_indices[i, j] = peak_idx
+                peak_masses[i, j] = all_peaks[i][peak_idx, 0]
+            else:
+                peak_indices[i, j] = -1
+                peak_masses[i, j] = np.nan
+        progress.update(i)
+
+# keep only the reference masses that actually have a 'hit' in at least one
+# of the samples.
+ref_hits             = list(np.where(np.any(peak_indices != -1, 0))[0])
+median_masses        = np.nanmedian(peak_masses[:, ref_hits], axis=0)
+peak_indices         = peak_indices[:, ref_hits]
+reference_mass_dlist = [reference_mass_dlist[i] for i in ref_hits]
+n_references         = len(ref_hits)
+
+#%%
+sys.stderr.write('\nCreating final matrix\n')
+
+# merged[i, j] will contain the intensity of the peak which was associated with
+# reference mass 'j' in sample 'i'. If there wasn't any close enough mass, 
+# we take the median mass of the ions associated with this reference mass
+# across all other samples, and find the ion closest to the median (even if 
+# it is not actually a peak).
+
+merged = np.zeros((n_samples, n_references), dtype=np.single)
+with ProgressBar(max_value=n_samples) as progress:
+    for i, dataset in enumerate(dataProfiles): # 13800 sec
+        for j, median_mass in enumerate(median_masses):
+            peak_idx = peak_indices[i, j]
+            if peak_idx != -1:
+                # if there is a peak associated with the metabolite,
+                # get the intensity of that peak
+                merged[i, j] = all_peaks[i][peak_idx, 1]
+            else:
+                # otherwise, get the intensity from the closest mz in the raw data
+                idx = np.argmin(np.abs(median_mass - dataset[:, 0]))
+                merged[i, j] = dataset[idx, 1]
+        progress.update(i)
+
+#%%
+if args.output_fname is None:
+    args.output_fname = args.exp_code + '.csv'
+
+sys.stderr.write('Writing results to output CSV file "%s" ... ' % args.output_fname)
+with open(args.output_fname, 'w') as fp:
+    csv_output = csv.writer(fp)
+    ref_keys = ['name', 'keggId', 'formula', 'mass']
+    csv_output.writerow(ref_keys + map(str, xrange(1, n_samples+1)))
+    for j, rdict in enumerate(reference_mass_dlist):
+        csv_output.writerow(map(rdict.get, ref_keys) + list(merged[:, j].flat))
+
+sys.stderr.write('[DONE]\n')
---
+++ fia/openbis.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 10:53:11 2015
+
+@author: noore
+"""
+
+import psycopg2
+import types
+import sys
+import numpy as np
+import itertools
+from collections import OrderedDict
+from progressbar import ProgressBar
+
+#% -------------------------------------------------------------------------
+#% fiaMLconfig - fiaexp database config file
+#% -------------------------------------------------------------------------
+#% [filedname]_table         database table containing the fields data 
+#%                           valid values :  data_set_properties
+#%                                           sample_properties
+#%
+#% [filedname]_pt_id         property type id
+#%                           valid values :  all integer
+#%
+#% [filedname]_customParser  anonymous MATLAB function to parse database
+#%                           content
+#%                           valid values :  empty
+#%                                           valid MATLAB function (@(x)
+#%                                           ...)
+#%
+#% [filedname]_type          data type to cast values into
+#%                           valid values :  numeric
+#%                                           alphanumeric
+#%
+#% -------------------------------------------------------------------------
+#% Sauerlab setup:
+#% -------------------------------------------------------------------------
+#% FIELD (openBIS)           TABLE                   PT_ID
+#% DS COMMENTS               data_set_properties     2
+#% DS SAMPLENAME             sample_properties       1
+#% DS SAMPLEPERTURBATION     sample_properties       3
+#% DS SAMPLETIME             sample_properties       4
+#% DS SAMPLEPOSITION         sample_properties       5
+#% DS SAMPLEAMOUNT           sample_properties       6
+#% DS SAMPLESPECIES          sample_properties       7
+#% DS FILENAME               data_set_properties     5
+#% -------------------------------------------------------------------------
+
+# openbis config
+host      = 'baobab.ethz.ch'  # hostname of the server running openBIS
+port      = 5432
+driver    = 'org.postgresql.Driver'                  # driver used to access openBIS
+use_ssl   = False                                    # Boolean specifying the use of SSl
+password  = 'openbisyeastx'                          # password for openBIS
+openbis_dsn   = 'openbis_productive'                     # data source name of the openBIS database
+openbis_login = 'openbis_readonly'                       # username for openBIS
+metabol_dsn   = 'metabol_productive'                     # data source name of the metabol database
+metabol_login = 'metabol_readonly'                       # username for openBIS
+exty_id = 1
+saty_id = 1
+dsty_id = 3
+
+#% DS COMMENTs / P[xxx]#[xxx][a-z]
+dsPlateStr_table = 'data_set_properties'
+dsPlateStr_pt_id = 2
+
+#% DS PERTURBATION
+dsSamplePerturbation_table = 'sample_properties'
+dsSamplePerturbation_pt_id = 3
+dsSamplePerturbation_customParser = ''
+dsSamplePerturbation_type = 'alphanumeric'
+
+#% DS SAMPLENAME
+dsSampleName_table = 'sample_properties'
+dsSampleName_pt_id = 1
+dsSampleName_customParser = ''
+dsSampleName_type = 'alphanumeric'
+
+#% DS SAMPLEAMOUNT
+dsSampleAmount_table = 'sample_properties'
+dsSampleAmount_pt_id = 6
+dsSampleAmount_customParser = ''
+dsSampleAmount_type = 'numeric'
+
+#% DS CONTROL
+dsControl_table = 'sample_properties'
+dsControl_pt_id = 7
+dsControl_customParser = '@parseControls'
+dsControl_type = 'numeric'
+
+#% DS USER1
+dsUser1_table = 'sample_properties'
+dsUser1_pt_id = 1
+dsUser1_customParser = ''
+dsUser1_type = 'alphanumeric'
+
+#% DS USER2
+dsUser2_table = 'sample_properties'
+dsUser2_pt_id = 4
+dsUser2_customParser = ''
+dsUser2_type = 'numeric'
+
+#% DS USER3
+dsUser3_table = 'sample_properties'
+dsUser3_pt_id = 6
+dsUser3_customParser = ''
+dsUser3_type = 'numeric'
+
+#% DS USER4
+dsUser4_table = 'data_set_properties'
+dsUser4_pt_id = 2
+dsUser4_customParser = ''
+dsUser4_type = 'alphanumeric'
+
+def list_to_comma_separated_string(l):
+    if type(l[0]) == types.StringType:
+        return ','.join(map(lambda s: "'" + s + "'", l))
+    else:
+        return ','.join(map(str, l))
+
+def download_data_profiles(exp_code):
+    conn_ob = psycopg2.connect(database=openbis_dsn,
+                               user=openbis_login,
+                               password=password,
+                               host=host,
+                               port=port)
+    conn_mb = psycopg2.connect(database=metabol_dsn,
+                               user=metabol_login,
+                               password=password,
+                               host=host,
+                               port=port)
+    cur_ob = conn_ob.cursor()
+    cur_mb = conn_mb.cursor()
+
+    # find the experiment ID (an openBIS internal ID, not the code that appears on
+    # the website)
+    cur_ob.execute("SELECT id FROM experiments WHERE code='%s';" % exp_code)
+    exp_id = cur_ob.fetchone()[0]
+    
+    sys.stderr.write('Downloading datasets associated to experiment %s\n' % exp_code)
+    
+    # get the conversion dictionary from 
+    cur_ob.execute("""SELECT   data.code, samples.code 
+                      FROM     data, samples
+                      WHERE    data.expe_id='%d' AND data.dsty_id=%d
+                        AND    data.samp_id = samples.id
+                      ORDER BY data.code""" % 
+                   (exp_id, dsty_id))
+    dsCode2smpCode = OrderedDict(cur_ob.fetchall())
+
+    cur_mb.execute("""SELECT   data_sets.perm_id, fia_ms_runs.id
+                      FROM     data_sets, fia_ms_runs
+                      WHERE    data_sets.perm_id in (%s)
+                        AND    data_sets.id = fia_ms_runs.ds_id
+                      ORDER BY data_sets.perm_id""" % 
+                   list_to_comma_separated_string(dsCode2smpCode.keys()))
+    dsCode2fiaId = OrderedDict(cur_mb.fetchall())
+
+    if dsCode2smpCode.keys() != dsCode2fiaId.keys():
+        raise Exception('Could not find all the datasets in openBIS, aborting...')
+    
+    with ProgressBar(max_value=len(dsCode2smpCode)) as progress:
+        dataProfiles = []
+        for i, fia_ms_run_id in enumerate(dsCode2fiaId.values()):
+            cur_mb.execute("""SELECT   mz, intensities 
+                              FROM     fia_profiles
+                              WHERE    fia_ms_run_id=%d 
+                              ORDER BY low_mz"""
+                           % fia_ms_run_id)
+            mz, intens = zip(*cur_mb.fetchall())
+            mz = map(lambda s: map(float, s.split(',')), mz)
+            mz = list(itertools.chain(*mz))
+        
+            intens = map(lambda s: map(float, s.split(',')), intens)
+            intens = list(itertools.chain(*intens))
+            profile = np.matrix([mz, intens], dtype=np.single).T
+            dataProfiles.append(profile)
+            progress.update(i)
+    
+    conn_ob.close()
+    conn_mb.close()
+    return dsCode2smpCode.keys(), dsCode2smpCode.values(), dataProfiles
